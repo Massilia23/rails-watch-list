5dfe4cfc70f99dfa7dcf3be30664b82416da2372
Massilia Bakhouche <massiliabakhouche@gmail.com>
2023-02-23 15:59:55 +0100

    watch

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,6 @@
 class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies,through: :bookmarks, dependent: :destroy
+  validates :name, uniqueness: true, presence: true
+  # validates :movie, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_102755) do
     t.string "title"
     t.text "overview"
     t.text "poster_url"
-    t.integer "rating"
+    t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
