e338c24021f96a60993f51e0b697ba4675e8dfaa
Massilia Bakhouche <massiliabakhouche@gmail.com>
2023-02-23 17:53:24 +0100

    watch

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,27 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    # @bookmark = Bookmark.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmarks_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirected_to bookmark_path(@bookmark)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirected_to bookmark_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmarks_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
 end
